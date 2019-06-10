6fad645770115c93896339dceae7fe04c948d7e3
汪成龙 <00237691@yto.net.cn>
2019-06-10 20:38:26 +0800

    测试，首次提交到本地

---
+++ src/main/java/com/example/testgit2019/Hello.java
@@ -0,0 +1,17 @@
+package com.example.testgit2019;  /**
+ * @title: Hello
+ * @projectName testgit2019
+ * @description: TODO
+ * @author hadoop
+ * @date 2019-6-1020:37
+ */
+
+/**
+ * @title: Hello
+ * @projectName testgit2019
+ * @description: TODO
+ * @author hadoop
+ * @date 2019-6-1020:37
+ */
+public class Hello {
+}
---
+++ src/main/java/com/example/testgit2019/Testgit2019Application.java
@@ -0,0 +1,13 @@
+package com.example.testgit2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testgit2019Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testgit2019Application.class, args);
+    }
+
+}
